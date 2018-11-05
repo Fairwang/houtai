3a0acd4bc8cd0bcfe925f6f320da2cc16598ce89
yumingxingxi127 <1273265323@qq.com>
2018-11-05 22:43:17 +0800

    理想金

--- other/pool.py
+++ other/pool.py
@@ -19,17 +19,11 @@
 #     p.close()
 #     p.join()
 #     print 'All subprocesses done.'
-#
-# # !/user/bin/python
-# #  -*-coding: utf-8-*-
-# import unittest
-# import time
-# from selenium.webdriver.common.by import By
-# from appium import webdriver
-# import time
-# import sys
 
-
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import unittest
 import time
 from selenium.webdriver.common.by import By
@@ -37,7 +31,7 @@ from appium import webdriver
 import time
 import sys
 from houtai.lk import iselementexist
-
+from jiandian.common import get_toast
 
 class zhifubao(unittest.TestCase):
     def setUp(self):
@@ -51,6 +45,7 @@ class zhifubao(unittest.TestCase):
         desired_caps['noReset'] = True
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
+        # self.get_toast=get_toast.get_toast(self.driver)
     def test_zhifubao(self):
         driver = self.driver
         driver.implicitly_wait(5)
@@ -66,30 +61,50 @@ class zhifubao(unittest.TestCase):
         # time.sleep(3)
         # print driver.contexts
         # time.sleep(3)
-
         # x = driver.get_window_size()['width']
         # y = driver.get_window_size()['height']
         # driver.swipe(int(x * 0.25), int(y * 0.25), int(x * 0.97), int(y * 0.25), 5000)
-        driver.tap([(500,1026),(600,1089)],2000)
-        time.sleep(10)
-        driver.tap([(420, 1020),(442, 1072)], 2000)#jinru kuanghuanyemian
         time.sleep(5)
-        driver.tap([(772, 1800),(793, 1826)], 2000)#jinru pingtai
-        time.sleep(2)
-        # driver.tap([(267, 1056),(277.1058)], 500)#xuanze dianpu
-        driver.tap([(734, 1222),(756, 1236)], 2000)#xuanze dianpu
-        time.sleep(2)
-        for i in range(3):
-            driver.tap([(509, 1123),(520, 11343)], 2000)#进入le店铺
-            time.sleep(2)
-            driver.tap([(467, 1375),(488, 1395)], 2000)
-            time.sleep(2)
-            driver.tap([(527, 1812),(549, 1833)], 2000)
-            time.sleep(2)
-        driver.tap([(968, 144),(980, 169)], 2000
-                   )#退出
-        time.sleep(2)
-
+        # driver.tap([(500,1020),(600,1086)],2000)
+        adb='adb shell input tap 524 1696'
+        os.system(adb)
+        time.sleep(10)
+        print driver.contexts
+        # adb1='adb shell input tap 400 1000'
+        # os.system(adb1)
+        # driver.tap([(335, 998),(500, 1172)], 4000)#点击狂欢城330 997  538  1206
+        # time.sleep(3)
+        # adb2='adb shell input tap 560 1782'#随意
+        # adb2='adb shell input tap 900 1853'#点击我的品牌728
+        # os.system(adb2)
+        # driver.tap([(730, 1800),(820, 1874)], 4000)#点击我的品牌728  1790   824  1874
+        # time.sleep(3)
+        while True:
+            # adb3 = 'adb shell input tap 794 1300'
+            adb3 = 'adb shell input tap 523 1806'
+            os.system(adb3)
+            print"选店铺"
+            time.sleep(5)
+            for i in range(2):
+                adb4 = 'adb shell input tap 505 1152'
+                os.system(adb4)
+                print"宝箱"
+                a=get_toast.get_toast(self.driver)
+                # driver.tap([(262, 1058), (500, 1065)], 2000)
+                time.sleep(2)
+                adb5 = 'adb shell input tap 400 1356'#进入包厢界面
+                os.system(adb5)
+                print"第一次"
+                # driver.tap([(380, 1356),(520, 1395)], 2000)#进入le店铺
+                time.sleep(2)
+                adb5 = 'adb shell input tap 552 1383'
+                os.system(adb5)
+                print"第二次"
+                print i
+                time.sleep(2)
+                # if get_toast.get_toast('本店今日投理想金次数已用完，先逛逛店铺吧'):
+                #     break
+            driver.tap([(968, 144),(980, 169)], 2000)#退出
     def tearDown(self):
         self.driver.quit()
 
