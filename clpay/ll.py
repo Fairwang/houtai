a736e447ce6cc2d40d6774cee9079d8bc6772201
yuemingxingxi127 <1273265323@qq.com>
2018-12-03 20:24:23 +0800

    no message

--- clpay/ll.py
+++ clpay/ll.py
@@ -92,25 +92,78 @@ import time
 #         # print len1
 #         f2.write('%s'%line)
 
-with open("E:\\zxtest\\lianjie2.txt", 'r') as f:
-    lines = f.readlines()  # 读取全部内容 ，并以列表方式返回
-    print lines
-    with open("E:\\zxtest\\lianjie4.txt", 'r') as f2:
-        lines2 = f2.readlines()  # 读取全部内容 ，并以列表方式返回
-        print lines2
-        for i in lines:
-            print i
-            for j in lines2:
-                print j
-                if j==i:
-                    print i
-                    with open("E:\\zxtest\\lianjie5.txt", 'w') as f5:
-                        f5.write('%s'%j)
-                else:
-                    with open("E:\\zxtest\\lianjie5.txt", 'w') as f5:
-                        f5.write('%s'%j)
-                    with open("E:\\zxtest\\lianjie5.txt", 'w') as f5:
-                        f5.write('%s'%i)
+import requests
+import time
+import json
+import re
+# driver=webdriver.Chrome()
+# with open("F:\\lianjie4.txt") as f:
+#     lines=f.readlines()
+#     i=0
+#     for url in lines:
+#         driver.get(url)
+#         js="window.open('https://www.baidu.com');"
+#         driver.execute_script(js)
+#         handles=driver.window_handles
+#         driver.switch_to.window(handles[-1])
+#         i+=1
+#         if i%20==0:
+#             driver = webdriver.Chrome()
+#     time.sleep(1000000)
+    #     url2=url.split("https")
+    #     print url2
+    #     with open("F:\\lianjie5.txt",'w') as f1:
+    #         for i in url2:
+    #             f1.write(i)
+
+# sc=[]
+# faileurl=[]
+#
+# with open("E:\\zxtest\\lianjie4.txt") as f:
+#     lines=f.readlines()
+#     i=0
+#     for url in lines:
+#         i+=1
+#         url2=url.strip()
+#         # print "url:%s"%url2
+#         r=requests.post(url2,)
+#         # time.sleep(2)
+#         r1=r.text
+#         # print r1
+#         if re.search("page_url",r1):
+#             # print "页面打开成功：%s"%url2
+#             # print r1
+#             r2=r1[-400:-1]#截取
+#             r3=r2.split(',')#分割出title
+#             # print r3
+#             r4=r3[-2].split("'")
+#             # print r4
+#             r5=r4[-2]
+#             # print r5
+#             # url2="u'"+r5+"'"
+#             sc1='<DT><A HREF='+'"'+url2+'"'+">"+r5+"</A>"
+#             sc.append(sc1)
+#         else:
+#             # print "页面打开失败：%s"%url2
+#             faileurl.append(url)
+#         if i==85:
+#             print "faileurl%s"%faileurl
+#             print "sc:%sc"%sc
+
+
+sc=[u'<DT><A HREF="https://www.cnblogs.com/shamo89/p/9032371.html">jmeter\u63a5\u53e3\u6d4b\u8bd5\u62a5java.net.SocketException: Socket closed\u9519\u8bef\u3002 - \u90a3\u5565\u5feb\u770b - \u535a\u5ba2\u56ed</</A>', u'<DT><A HREF="https://testerhome.com/topics/12474">TesterHome</</A>', u'<DT><A HREF="https://www.cnblogs.com/imlvbu/p/7127940.html">\u3010\u4eb2\u6d4b\u3011appium_v1.4.16\u7248\u672c\u81ea\u52a8\u5316\u9002\u914dandroid7.0\u7cfb\u7edf - imlvbu - \u535a\u5ba2\u56ed</</A>', u'<DT><A HREF="https://www.cnblogs.com/windhome/p/7615251.html">appium\u542f\u52a8\u8fd0\u884clog\u5206\u6790 - \u98ce\u541f\u7684\u5c0f\u5c4b - \u535a\u5ba2\u56ed</</A>', u'<DT><A HREF="https://www.cnblogs.com/fnng/p/8486863.html">uiautomator2 \u4f7f\u7528Python\u6d4b\u8bd5 Android\u5e94\u7528 - \u866b\u5e08 - \u535a\u5ba2\u56ed</</A>', u'<DT><A HREF="http://www.cnblogs.com/fnng/p/5370433.html">\u5173\u4e8e\u81ea\u52a8\u5316\u6d4b\u8bd5\u7684\u8bef\u533a(\u4e00) - \u866b\u5e08 - \u535a\u5ba2\u56ed</</A>', u'<DT><A HREF="https://www.cnblogs.com/rookie-c/p/5755093.html">web\u6d4b\u8bd5\u4e0eapp\u6d4b\u8bd5\u7684\u533a\u522b - Rookie_C - \u535a\u5ba2\u56ed</</A>', u'<DT><A HREF="https://www.cnblogs.com/xueweihan/p/5207959.html">[python]decimal\u5e38\u7528\u64cd\u4f5c\u548c\u9700\u8981\u6ce8\u610f\u7684\u5730\u65b9 - \u524a\u5fae\u5bd2 - \u535a\u5ba2\u56ed</</A>', u'<DT><A HREF="https://www.cnblogs.com/rookie-c/p/5755093.html">web\u6d4b\u8bd5\u4e0eapp\u6d4b\u8bd5\u7684\u533a\u522b - Rookie_C - \u535a\u5ba2\u56ed</</A>', u'<DT><A HREF="http://bbs.tianya.cn/post-itinfo-437263-1.shtml">APP\u6d4b\u8bd5\u600e\u4e48\u505a\uff0cAPP\u6d4b\u8bd5\u7684\u6700\u4f73\u65b9\u6cd5(\u8f6c\u8f7d)_\uff29\uff34\u89c6\u754c_\u8bba\u575b_\u5929\u6daf\u793e\u533a</</A>']
+
+
+for i in sc:
+    print i
+
+
+
+# for i in sc:
+#     print i
+
+# print 988-189
+
 # a=['https://blog.csdn.net/test94/article/details/51519789   Android\xd7\xd4\xb6\xaf\xbb\xaf\xb2\xe2\xca\xd4Python\n','\xa3\xa8\xd4\xad\xb4\xb4\xa3\xa9\xc8\xe7\xba\xce\xb6\xd4APP\xb7\xfe\xce\xf1\xb6\xcb\xbd\xf8\xd0\xd0\xd1\xb9\xc1\xa6\xb2\xe2\xca\xd4\n']
 # t=a[0]
 # # print y
--- clpay/sponsor_pay.py
+++ clpay/sponsor_pay.py
@@ -18,56 +18,64 @@ class clpay1():
             return flag
     def clpay(self):
         # self.driver=webdriver.Chrome()
-        self.driver.get('https://pay.hongnaga.com/?debug=true')
+        # self.driver.get('https://pay.hongnaga.com/?debug=true')
+
         # driver.get("http://pay.frp.tinywan.top/?debug=true")
-        # driver.get("https://pay.hongnaga.com/?debug=true")
+        driver.get("https://testpay.hongnaga.com/?debug=true")
         # h=driver.current_window_handle
         # print h
         a=self.driver.switch_to.alert
         a.send_keys("123456778")
         a.accept()
-        self.driver.implicitly_wait(2)
-    #商户ID
-        self.driver.find_element_by_xpath("//*[@name='mch_id']").clear()
-        self.driver.find_element_by_xpath("//*[@name='mch_id']").send_keys("12001")
 
-    # 支付方式
-        pay_type=Select(self.driver.find_element_by_id("pay_type"))
-        self.driver.find_element_by_id("pay_type").click()
-        pay_type.select_by_value("11")  # 商户代付
+        prices=[30,10,15,20]
+        for price in prices:
+            self.driver.implicitly_wait(2)
+        #商户ID
+            self.driver.find_element_by_xpath("//*[@name='mch_id']").clear()
+            self.driver.find_element_by_xpath("//*[@name='mch_id']").send_keys("12001")
 
-    # 金额
-        self.driver.find_element_by_name("price").clear()
-        self.driver.find_element_by_name("price").send_keys("10")
+        # 支付方式
+            pay_type=Select(self.driver.find_element_by_id("pay_type"))
+            self.driver.find_element_by_id("pay_type").click()
+            pay_type.select_by_value("11")  # 商户代付
 
-    # 银行编码
-        self.driver.find_element_by_xpath("//*[@name='bank_code']").clear()
-        self.driver.find_element_by_xpath("//*[@name='bank_code']").send_keys("01050000")
+        # 金额
+            self.driver.find_element_by_name("price").clear()
+            self.driver.find_element_by_name("price").send_keys(price)
+        #代理商id
+            self.driver.find_element_by_name("account_id").clear()
+            self.driver.find_element_by_name("account_id").send_keys("201806")
 
-    # 姓名
-        self.driver.find_element_by_xpath("//*[@name='username']").clear()
-        self.driver.find_element_by_xpath("//*[@name='username']").send_keys("付贵炉")
-    # 银行卡号
-        self.driver.find_element_by_xpath("//*[@name='card_no']").clear()
-        self.driver.find_element_by_xpath("//*[@name='card_no']").send_keys("6217001540022416380")
+        # 银行编码
+            self.driver.find_element_by_xpath("//*[@name='bank_code']").clear()
+            self.driver.find_element_by_xpath("//*[@name='bank_code']").send_keys("01050000")
 
-        time.sleep(2)
-        self.driver.find_element_by_id("pay").click()
-        self.driver.implicitly_wait(5)
-        time.sleep(1)
+        # 姓名
+            self.driver.find_element_by_xpath("//*[@name='username']").clear()
+            self.driver.find_element_by_xpath("//*[@name='username']").send_keys(u"付贵炉")#UnicodeDecodeError: 'utf8' codec can't decode byte 0xe4 in position 0: unexpected end of data
+        # 银行卡号
+            self.driver.find_element_by_xpath("//*[@name='card_no']").clear()
+            self.driver.find_element_by_xpath("//*[@name='card_no']").send_keys("6217001540022416380")
 
-        windows = self.driver.window_handles#获取所有windows窗口句柄
-        time.sleep(1)
-        self.driver.switch_to.window(windows[0])#返回发起界面
-        time.sleep(1)
-        zf11="支付完成"
-        clpay=clpay1(self.driver)
-        if clpay.isElementExist(zf11):
-            self.driver.find_element_by_link_text("支付完成").click()
-        else:
-            pass
-        time.sleep(3)
+            time.sleep(2)
+            self.driver.find_element_by_id("pay").click()
+            self.driver.implicitly_wait(5)
+            time.sleep(1)
+
+            windows = self.driver.window_handles#获取所有windows窗口句柄
+            time.sleep(1)
+            self.driver.switch_to.window(windows[0])#返回发起界面
+            time.sleep(3)
+            zf11="支付完成"
+            clpay=clpay1(self.driver)
+            if clpay.isElementExist(zf11):
+                self.driver.find_element_by_link_text("支付完成").click()
+            else:
+                pass
+            time.sleep(2)
+
 driver=webdriver.Chrome()
 daifu=clpay1(driver)
 daifu.clpay()
-
+time.sleep(1000)
