c8efbdfe3f167f973e21936a69b64f40a6711a29
yumingxingxi127 <1273265323@qq.com>
2019-03-23 18:01:38 +0800

    cpayadd

--- cpay/erweima_pay/cpay_debug2.py
+++ cpay/erweima_pay/cpay_debug2.py
@@ -21,7 +21,7 @@ i = 1
     # price = f1.readline()
     # if price=='':
     #     break
-prices=[1,1.01,1.02]
+prices=[1,1.01,2,25,15]
 for price in prices:
     price = str(price)
     time.sleep(3)
@@ -31,7 +31,7 @@ for price in prices:
     # driver.find_element_by_name("sub_mch_id").clear()
     # driver.find_element_by_name("sub_mch_id").send_keys(6020)
     pay_type=Select(driver.find_element_by_id("pay_type"))
-    pay_type.select_by_value("13")
+    pay_type.select_by_value("11")
     driver.find_element_by_name("price").clear()#金额
     driver.find_element_by_name("price").send_keys(price)
     driver.find_element_by_id("pay").click()
